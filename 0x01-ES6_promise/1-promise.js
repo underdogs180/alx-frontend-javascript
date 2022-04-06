4fcf71ea858ccfcf04c93da9a4428a5039ec21a6
underdogs180 <asadirshad38@gmailcom>
2022-04-06 14:01:36 +0000

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
+  return new Promise((resol, fail) => {
+    if (success) {
+      resol({
 	status: 200,
 	body: 'Success',
       });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      fail(new Error('The fake API is not working currently'));
     }
   });
 }
