aa2494ed6d9415a6832e708a8b689f48a150447b
underdogs180 <asadirshad38@gmailcom>
2022-04-12 06:06:25 +0000

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,13 @@
-export default function getStudentsByLocation(array, city, grad) {
-  return array
-    .filter((i) => i.location === city)
-    .map((student) => {
-      const gradeI = grad
-	.filter((i) => i.studentId === student.id)
-	.map((x) => x.grade)[0];
-      const grade = gradeI || 'N/A';
-      return { ...student, grade };
-    });
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  // check arg is an array
+  if (Object.getPrototypeOf(students) !== Array.prototype) {
+    return [];
+  }
+  if (Object.getPrototypeOf(newGrades) !== Array.prototype) {
+    return [];
+  }
+  return students.filter((student) => student.location === city).map((student) => {
+    const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+    return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
+  });
 }
